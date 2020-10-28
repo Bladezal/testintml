9e316cf20d0a7339443ffddb429b422ce4aad701
Daniel Cazal <bladezal@gmail.com>
2020-10-27 21:11:46 -0300

    Blade templates

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AccountsController extends Controller
-{
-    //
+class AccountsController extends Controller{
+    
 }
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+    <div class="navbar-inner">
+        <a id="logo" href="/">Single Malt</a>
+        <ul class="nav">
+            <li><a href="/">Home</a></li>
+            <li><a href="#">Pedidos</a></li>
+            <li><a href="#">Cuentas</a></li>
+            <li><a href="#">Productos</a></li>
+        </ul>
+    </div>
+</div>
