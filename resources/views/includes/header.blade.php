bd1f3a3788a0412e7e4234da730293bcf90b247c
Daniel Cazal <bladezal@gmail.com>
2020-10-27 21:50:54 -0300

    Blade templates

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,11 +1,9 @@
-<div class="navbar">
-    <div class="navbar-inner">
-        <a id="logo" href="/">Single Malt</a>
-        <ul class="nav">
-            <li><a href="/">Home</a></li>
-            <li><a href="#">Pedidos</a></li>
-            <li><a href="#">Cuentas</a></li>
-            <li><a href="#">Productos</a></li>
-        </ul>
-    </div>
-</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" id="logo" href="/">Single Malt</a>
+    <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Pedidos</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Cuentas</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Productos</a></li>
+    </ul>
+</nav>
