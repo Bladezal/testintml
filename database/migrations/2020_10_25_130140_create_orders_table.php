c4f0f69f2faccc8599d31917969ef19a473d02e1
Daniel Cazal <bladezal@gmail.com>
2020-11-05 23:13:44 -0300

    Ajustes solicitados al migrations de orders

--- database/migrations/2020_10_25_130140_create_orders_table.php
+++ database/migrations/2020_10_25_130140_create_orders_table.php
@@ -18,10 +18,10 @@ class CreateOrdersTable extends Migration
             $table->bigInteger('id_order');
             $table->dateTime('date_created_order');
             $table->float('total_amount_order',8,2);
-            $table->char('reason_order',250);
+            $table->char('reason_order',250)->nullable();
             $table->char('first_name_order',100);
             $table->char('last_name_order',100);
-            $table->char('shipping_type_order',100);
+            $table->char('shipping_type_order',100)->nullable();
             $table->text('detail_order');
             $table->text('notes',500)->nullable();
             $table->foreignId('intl_status')->nullable();
