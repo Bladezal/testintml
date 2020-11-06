18dcbb6580e327aeb6f0901e96a540335a9513e2
Daniel Cazal <bladezal@gmail.com>
2020-11-06 00:23:18 -0300

    Ajustes solicitados al migrations de orders

--- resources/views/pages/orders/index.blade.php
+++ resources/views/pages/orders/index.blade.php
@@ -13,30 +13,28 @@
         <tr>
             <th>Id Pedido</th>
             <th>Fecha Pedido</th>
-            <th>Fecha Finalizaci&oacute;n</th>
-            <th>Estado Pedido</th>
             <th>Monto Pedido</th>
-            <th>Moneda</th>
+            <th>T&iacute;tulo Pedido</th>
             <th>Nombre Cliente</th>
             <th>Apellido Cliente</th>
-            <th>Id Env&iacute;o</th>
+            <th>Tipo Env&iacute;</th>
             <th>Notas</th>
             <th>Estado Interno</th>
+            <th>Acciones</th>
         </tr>
         @isset($orders)
             @foreach ($orders as $order)
                 <tr>
                     <td>{{$order->id_order}}</td>
                     <td>{{$order->date_created_order}}</td>
-                    <td>{{$order->date_closed_order}}</td>
-                    <td>{{$order->status_order}}</td>
                     <td>{{$order->total_amount_order}}</td>
-                    <td>{{$order->currency_id}}</td>
+                    <td>{{$order->reason_order}}</td>
                     <td>{{$order->first_name_order}}</td>
                     <td>{{$order->last_name_order}}</td>
-                    <td>{{$order->shipping_id_order}}</td>
+                    <td>{{$order->shipping_type_order}}</td>
                     <td>{{$order->notes}}</td>
                     <td>{{$order->intl_status}}</td>
+                    <td></td>
                 </tr>
             @endforeach
         @endisset
