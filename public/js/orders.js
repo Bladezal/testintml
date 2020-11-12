dc6a03ce00d529a901fdc7c98ac81abd53f14020
Daniel Cazal <bladezal@gmail.com>
2020-11-12 20:41:08 -0300

    Cambios al actualizar datos del pedido.

--- public/js/orders.js
+++ public/js/orders.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
                     status: $("#orderstatus").val()},
             success: function(data) {
                 if (data.success) {
-                    $('#modalDetalle').modal('toggle');
+                    $('#modalDetalle').modal('hide');
                     location.reload();
                 }
             }
