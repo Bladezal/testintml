43844c20ef9e910ef2641be9673bd43f7c45cec3
Daniel Cazal <bladezal@gmail.com>
2020-11-15 23:47:17 -0300

    Cambios al actualizar datos del pedido y authorization, refresh token.

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -54,6 +54,8 @@ class OrdersController extends Controller{
                 /*$shipping = Http::withToken($account->access_token)
                             ->get(Config::get('constants.base_ML_URI').'/shipments/'.$order->shipping->id);*/
                 $shipping_detail = json_decode($this->mlGetRequest($account->access_token,'/shipments/',$order->shipping->id));//json_decode($shipping);
+                print_r($shipping_detail);
+                die('---');
                 $orden->shipping_type_order = $shipping_detail->shipping_option->name;
                 $orden->save();
             }
